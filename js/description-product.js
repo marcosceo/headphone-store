51efe45bbb714f6c6a59932714314443fce426a4
Marcos Ceo <marcos.ceo@outlook.com>
2019-04-08 13:44:46 -0300

    Desafio aula 16 e refatoração do código JS de avaliação(estrelas)

--- js/description-product.js
+++ js/description-product.js
@@ -1,64 +1,83 @@
 const $heart = document.querySelector(".-heart");
-const $stars = document.querySelectorAll(".star");
+let $stars = document.querySelectorAll(".star");
+
+console.log($stars);
 
 $heart.addEventListener("click", active);
 
-$stars.forEach(function ($star) {
-    $star.addEventListener("click", activeStar);
-})
+for (let e = 0; e < $stars.length; e++) {
+    $stars[e].addEventListener("click", function () {
+        for (i = 4; i >= 0; i--) {
+            $stars[i].classList.remove("-active");
+        }
+        for (i = e; i >= 0; i--) {
+            $stars[i].classList.add("-active");
+        }
+    })
+}
 
 function active() {
     this.classList.toggle("-active");
 }
 
-function activeStar() {
-    if (this.classList.contains("-first")) {
-        if ($stars[0].classList.contains("-active")) {
-            for (let i = 4; i >= 1; i--) {
-                $stars[i].classList.remove("-active");
-            }
-        } else {
-            this.classList.toggle("-active");
-        }
-    } else if (this.classList.contains("-second")) {
-        if ($stars[1].classList.contains("-active")) {
-            for (let i = 4; i >= 2 ; i--) {
-                $stars[i].classList.remove("-active");
-            }
-        } else {
-            for (let i = 1; i >= 0; i--) {
-                $stars[i].classList.add("-active");
-            }
-        }
-    } else if (this.classList.contains("-third")) {
-        if ($stars[2].classList.contains("-active")) {
-            for (let i = 4; i >= 3; i--) {
-                $stars[i].classList.remove("-active");
-            }
-        } else {
-            for (let i = 2; i >= 0; i--) {
-                $stars[i].classList.add("-active");
-            }
-        }
-    } else if (this.classList.contains("-fourth")) {
-        if ($stars[3].classList.contains("-active")) {
-            for (let i = 4; i >= 4; i--) {
-                $stars[i].classList.remove("-active");
-            }
-        } else {
-            for (let i = 3; i >= 0; i--) {
-                $stars[i].classList.add("-active");
-            }
-        }
-    } else if (this.classList.contains("-fifth")) {
-        if ($stars[4].classList.contains("-active")) {
-            for (let i = 4; i > 4; i--) {
-                $stars[i].classList.remove("-active");
-            }
-        } else {
-            for (let i = 4; i >= 0; i--) {
-                $stars[i].classList.add("-active");
-            }
-        }
-    }
-}
+
+
+
+// Primeira versao do codigo(para funcionar tem que adicionar as classes first-fifth no HTML):
+
+// $stars.forEach(function ($star) {
+//     $star.addEventListener("click", activeStar);
+// })
+
+// function activeStar() {
+//     if (this.classList.contains("-first")) {
+//         if ($stars[0].classList.contains("-active")) {
+//             for (let i = 4; i >= 1; i--) {
+//                 $stars[i].classList.remove("-active");
+//             }
+//         } else {
+//             this.classList.toggle("-active");
+//         }
+//     } else if (this.classList.contains("-second")) {
+//         if ($stars[1].classList.contains("-active")) {
+//             for (let i = 4; i >= 2 ; i--) {
+//                 $stars[i].classList.remove("-active");
+//             }
+//         } else {
+//             for (let i = 1; i >= 0; i--) {
+//                 $stars[i].classList.add("-active");
+//             }
+//         }
+//     } else if (this.classList.contains("-third")) {
+//         if ($stars[2].classList.contains("-active")) {
+//             for (let i = 4; i >= 3; i--) {
+//                 $stars[i].classList.remove("-active");
+//             }
+//         } else {
+//             for (let i = 2; i >= 0; i--) {
+//                 $stars[i].classList.add("-active");
+//             }
+//         }
+//     } else if (this.classList.contains("-fourth")) {
+//         if ($stars[3].classList.contains("-active")) {
+//             for (let i = 4; i >= 4; i--) {
+//                 $stars[i].classList.remove("-active");
+//             }
+//         } else {
+//             for (let i = 3; i >= 0; i--) {
+//                 $stars[i].classList.add("-active");
+//             }
+//         }
+//     } else if (this.classList.contains("-fifth")) {
+//         if ($stars[4].classList.contains("-active")) {
+//             for (let i = 4; i > 4; i--) {
+//                 $stars[i].classList.remove("-active");
+//             }
+//         } else {
+//             for (let i = 4; i >= 0; i--) {
+//                 $stars[i].classList.add("-active");
+//             }
+//         }
+//     }
+// }
+
